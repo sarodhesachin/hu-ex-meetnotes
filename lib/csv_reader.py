03d262be7f9c3117bce02719ed072a0a236da7e5
sarodhesachin <sachin.s@hashedin.com>
2019-12-27 14:36:25 +0530

    last commit
    Signed-off-by: sarodhesachin <sachin.s@hashedin.com>

--- lib/csv_reader.py
+++ lib/csv_reader.py
@@ -7,10 +7,33 @@ class CSV_Reader(object):
         self.file_path = file_path
 
         with open(self.file_path, 'r') as csv_file:
-            self.headers = next(csv.reader(csv_file))
+            header = next(csv.reader(csv_file))[0]
+            self.headers = header.split('\t')
+
+    # def csv_generator(self):
+    #     with open(self.file_path, 'r') as csv_file:
+    #         reader = csv.reader(csv_file)
+    #         for row in reader:
+    #             yield row
 
     def csv_generator(self):
         with open(self.file_path, 'r') as csv_file:
-            reader = csv.reader(csv_file)
+            reader = csv.DictReader(csv_file)
             for row in reader:
-                yield row
+                key = next(iter(row))
+                value = row.get(key, '')
+
+                key = key.split('\t')
+                value = value.split('\t')
+                yield dict(zip(key, value))
+
+    def get_columns(self, column):
+        col_position = self.headers.index(column)
+        items = list()
+        for row in self.csv_generator():
+            row_content = row[0].split('\t')
+            if len(row_content) > col_position:
+                items.append(row_content[col_position])
+
+        # Returning all items except header
+        return items[1:]
--- meetnotes.py
+++ meetnotes.py
@@ -1,3 +1,5 @@
+import re
+
 from common import constants as CONSTANTS
 from lib.csv_reader import CSV_Reader
 
@@ -14,12 +16,29 @@ class MeetNotes(object):
         pass
 
     def get_most_viewed_pages(self):
-        pass
+
+        for record in self.csv.csv_generator():
+            # print(re.findall(r'(/+[+]?[a-z]+)+', record['location']))
+            record = record['location'].split('?')[0]
+            print(record)
+            print(re.findall(r'', record))
+
+        # values = self.csv.get_columns('location')
+        # unique_values = {'/', }
+        # for value in values:
+        #     temp = re.findall(r'/[+]?[a-z]*/?', value)
+        #     if len(temp) > 0:
+        #         unique_values.add(temp[0])
+        #
+        # # unique_values = {re.findall(r'/', value) for value in values}
+        # print(values[81])
+        # print(re.findall(r'/[+]?[a-z]*/?', values[81]))
+        # print(unique_values)
+        # for row in self.csv.csv_generator1():
+        #     print(row)
 
 
 if __name__ == "__main__":
     print("Running meet notes")
     m = MeetNotes()
-
-    for row in m.csv.csv_generator():
-        print(row)
+    m.get_most_viewed_pages()
